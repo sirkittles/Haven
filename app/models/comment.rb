4f877d58d6add3e4c9f96fcdce240561ade20b64
Diane So <so.dianeyj@gmail.com>
2020-10-20 18:53:25 -0400

    models edited

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # validates :username, presence: true
+  # validates :content, prsences: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_and_belongs_to_many :hashtags
 end
