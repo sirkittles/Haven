b5f5307000db094adfd012f765492e8495eaed6a
Diane So <so.dianeyj@gmail.com>
2020-10-21 00:16:29 -0400

    changed post content data type from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_224000) do
+ActiveRecord::Schema.define(version: 2020_10_21_034955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_224000) do
 
   create_table "posts", force: :cascade do |t|
     t.string "img_url"
-    t.string "content"
+    t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
