9961f0b31bfa9c5c7036a26f58989247db6ce5e2
Diane So <so.dianeyj@gmail.com>
2020-10-22 02:00:22 -0400

    register account added

--- client/src/screens/register-account/RegisterAccount.jsx
+++ client/src/screens/register-account/RegisterAccount.jsx
@@ -1,8 +1,72 @@
 import React from 'react';
 
-const RegisterAccount = () => {
-  return (
+const RegisterAccount = (props) => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: ""
+  });
+  const { username, email, password } = formData;
+  const { error, handleRegister } = props;
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleRegister(formData);
+      }}>
+        <label>
+          First Name:
+           <input
+            type="text"
+            value={firstName}
+            name="First Name"
+            onChange={handleChange}
+          />
+        </label>
+        <label>Last Name:
+         <input
+            type="text"
+            value={lastName}
+            name="Last Name"
+            onChange={handleChange}
+          />
+        </label>
+        <label>Username:
+         <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={handleChange}
+          />
+        </label>
+        <label>Email:
+         <input
+            type="text"
+            value={email}
+            name="Email"
+            onChange={handleChange}
+          />
+        </label>
+        <label>Password:
+         <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={handleChange}
+          />
+        </label>
+        <button>Sign Up</button>
+      </form>
+    </div>
   )
 }
 
