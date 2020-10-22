316c136e673f5d8a95b0b764389cb99e7efb780e
Diane So <so.dianeyj@gmail.com>
2020-10-22 01:37:08 -0400

    rough landing page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
---
+++ client/src/screens/landing-page/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        <h1>Welcome!</h1>
+        <h4>Show off your room</h4>
+        <Link to="/create-account">
+          <button>Join Us</button>
+        </Link>
+      </div>
+      <div>
+        <p>Already have an account?</p><Link to='/login'>Sign in</Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
