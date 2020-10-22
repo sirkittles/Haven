87d0b2746b64476dd85d605ba63268c9be2c8b44
Diane So <so.dianeyj@gmail.com>
2020-10-22 01:39:14 -0400

    routed to landingpage in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import LandingPage from './screens/landing-page/LandingPage';
 
 function App() {
   return (
     <div className="App">
-
+      <Switch>
+        <Route exact path='/'>
+          <LandingPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
