3479b22667906322f26f5eda1958818cdf4cc449
Diane So <so.dianeyj@gmail.com>
2020-10-22 14:06:43 -0400

    basic layout complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { Route, Switch, useHistory } from 'react-router-dom';
-import './App.css';
-import { registerUser, loginUser, removeToken } from './services/auth';
-import LandingPage from './screens/landing-page/LandingPage';
-import RegisterAccount from './screens/register-account/RegisterAccount';
-import LoginPage from './screens/login-page/LoginPage';
-import HomePage from './screens/home-page/HomePage';
+import React, { useState } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import "./App.css";
+import { registerUser, loginUser, removeToken } from "./services/auth";
+import LandingPage from "./screens/landing-page/LandingPage";
+import RegisterAccount from "./screens/register-account/RegisterAccount";
+import LoginPage from "./screens/login-page/LoginPage";
+import HomePage from "./screens/home-page/HomePage";
+import Layout from "./components/shared/layout/Layout";
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -14,37 +15,42 @@ function App() {
   const handleLogin = async (loginData) => {
     const userData = await loginUser(loginData);
     setCurrentUser(userData);
-    history.push('/homepage')
-  }
+    history.push("/homepage");
+  };
 
   const handleRegister = async (registerData) => {
     const userData = await registerUser(registerData);
     setCurrentUser(userData);
-    history.push('/homepage')
-  }
+    history.push("/homepage");
+  };
 
   const handleLogout = () => {
     setCurrentUser(null);
-    localStorage.removeItem('authToken');
+    localStorage.removeItem("authToken");
     removeToken();
-  }
+  };
 
   return (
     <div className="App">
-      <Switch>
-        <Route exact path='/'>
-          <LandingPage />
-        </Route>
-        <Route path='/register-account'>
-          <RegisterAccount handleRegister={handleRegister} />
-        </Route>
-        <Route path='/login'>
-          <LoginPage handleLogin={handleLogin} />
-        </Route>
-        <Route path='/homepage'>
-          <HomePage />
-        </Route>
-      </Switch>
+      <Layout
+        currentUser={currentUser}
+        handleLogout={handleLogout}
+      >
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/register-account">
+            <RegisterAccount handleRegister={handleRegister} />
+          </Route>
+          <Route path="/login">
+            <LoginPage handleLogin={handleLogin} />
+          </Route>
+          <Route path="/homepage">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
--- client/src/components/shared/layout/Layout.jsx
+++ client/src/components/shared/layout/Layout.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Layout = (props) => {
+  const { currentUser, handleLogout } = props;
   return (
     <div className="layout">
-      <Header />
+      <Header
+        currentUser={currentUser}
+        handleLogout={handleLogout}
+      />
       <div className="layout-children">
         {props.children}
       </div>
