3d9b70bc986b690be166f812837b4953fe37aa75
Diane So <so.dianeyj@gmail.com>
2020-10-22 02:37:53 -0400

    fixed loginUser in auth.js, login also tested and working

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,7 +1,7 @@
 import api from './apiConfig';
 
 export const loginUser = async (loginData) => {
-  const resp = await api.post('/auth/login', { user: loginData });
+  const resp = await api.post('/auth/login', { authentication: loginData });
   localStorage.setItem('authToken', resp.data.token);
   api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
   return resp.data.user;
@@ -17,7 +17,7 @@ export const registerUser = async (registerData) => {
 export const verifyUser = async () => {
   const token = localStorage.getItem('authToken');
   if (token) {
-    api.defaults.headers.common.authorization = `Bearer ${token}`;
+    api.defaults.headers.common.authorization = `Bearer ${token}`
     const resp = await api.get('/auth/verify');
     return resp.data;
   }
