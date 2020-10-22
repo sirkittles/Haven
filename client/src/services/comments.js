727274bfc60c5b8f405a9e935978229089bc27ce
Diane So <so.dianeyj@gmail.com>
2020-10-22 01:22:58 -0400

    comments services added

--- client/src/services/comments.js
+++ client/src/services/comments.js
@@ -1,2 +1,26 @@
 import api from './apiConfig';
 
+export const getComments = async () => {
+  const resp = await api.get(`/comments`);
+  return resp.data;
+};
+
+export const getOneComment = async (id) => {
+  const resp = await api.get(`/comments/${id}`);
+  return resp.data;
+};
+
+export const postComment = async (commentData) => {
+  const resp = await api.post(`/comments`, { comment: commentData });
+  return resp.data;
+};
+
+export const putComment = async (id, commentData) => {
+  const resp = await api.put(`/comments/${id}`, { comment: commentData });
+  return resp.data;
+};
+
+export const destroyComment = async (id) => {
+  const resp = await api.delete(`/comments/${id}`);
+  return resp;
+}
