7d2e42ca73c231b6cb972024b50ee07e04d29ee2
Diane So <so.dianeyj@gmail.com>
2020-10-22 21:10:18 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Comment.destroy_all
 Post.destroy_all
 User.destroy_all
 
-@user = User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
+@user1 = User.create!(username: 'admin', email: 'admin@email.com', password: '123456')
+@user2 = User.create!(username: 'tester', email: 'tester@email.com', password: 'tester123')
+puts "#{User.count} users created"
 
-@post = Post.create!(img_url: 'http://www.fakeimage.com/fake', content: 'this is fake content for my fake post.', user_id: 1)
+@post1 = Post.create!(img_url: 'http://www.fakeimage.com/fake', content: 'this is fake content for my fake post.', user_id: @user1.id)
+@post2 = Post.create!(img_url: 'https://i.redd.it/mlujr8os37u51.jpg', content: 'My minimalistic living space - Chicago, IL', user_id: @user2.id)
 puts "#{Post.count} posts created"
+
+@comment1 = Comment.create!(content: 'Wow this is amazing', user_id: @user1.id, post_id: @post2.id)
+@comment2 = Comment.create!(content: 'image link is broken duuude', user_id: @user2.id, post_id: @post1.id)
+puts "#{Comment.count} comments created"
